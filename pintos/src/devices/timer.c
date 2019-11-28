615f984b03b573be5b66bfc6a941e6a7d7ff41d2
asomeJay <museum114@naver.com>
2019-11-28 03:37:28 -0800

    Project 3 : Alaram Complete

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -7,6 +7,7 @@
 #include "threads/interrupt.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
+
   
 /* See [8254] for hardware details of the 8254 timer chip. */
 
@@ -23,18 +24,20 @@ static int64_t ticks;
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
-
+static struct list sleep_list;
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
+//list_init(&sleep_list);
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
 timer_init (void) 
 {
+  list_init(&sleep_list);
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -92,8 +95,22 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
+
+  struct thread * cur = thread_current();
+  enum intr_level old_level;
+  
+  ASSERT (!intr_context());
+     
+  old_level = intr_disable();
+  cur->wake_up_time = start + ticks;
+  list_push_back(&sleep_list, &cur->elem);
+//  cur->status = THREAD_BLOCKED;
+  thread_block();
+  intr_set_level(old_level);
+
+/*
   while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+    thread_yield ();*/
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -171,6 +188,22 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+  
+  struct thread* temp_thread = NULL;
+  struct list_elem * temp_elem = NULL;
+ /* 그냥 for 문에 temp_elem = list_next(temp_elem)을 하니까 Kernel PANIC이 떳다. */ 
+  for(temp_elem = list_begin(&sleep_list); temp_elem != list_end(&sleep_list);){
+  temp_thread = list_entry(temp_elem, struct thread, elem);
+  
+  if(temp_thread->wake_up_time <= ticks){
+    temp_elem = list_remove(temp_elem);
+    thread_unblock(temp_thread);
+  }
+  else{
+    temp_elem = list_next(temp_elem);
+  }
+ 
+}
   thread_tick ();
 }
 
--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -89,7 +89,7 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+    int wake_up_time; 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 #ifdef USERPROG
